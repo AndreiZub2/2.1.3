f55dd1b6ac48d7eae12f84c839be24c1f95ac562
AndreeiZub2 <andrei_zubkovr@mail.ru>
2023-08-26 22:35:44 +0400

    jv

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("Cat");
+        System.out.println(catBean.getMessage());
+        Cat catBean1 =
+                (Cat) applicationContext.getBean("Cat");
+        System.out.println(catBean1.getMessage());
+
+
+        System.out.println(bean.equals(bean1));
+        System.out.println(catBean.equals(catBean1));
     }
 }
